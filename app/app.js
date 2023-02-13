457f53c6218276cf5a69b24851d33df65c7f765f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-02-13 16:07:33 +0530

    angular

--- app/app.js
+++ app/app.js
@@ -5,18 +5,28 @@ var phonecatApp = angular.module('phonecatApp', []);
 phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
   $scope.myName = 'Vaman';
 
-  // $scope.phones = [
-  //   {
-  //     name: 'Nexus S',
-  //     snippet: 'Fast just got faster with Nexus S.'
-  //   }, {
-  //     name: 'Motorola XOOM™ with Wi-Fi',
-  //     snippet: 'The Next, Next Generation tablet.'
-  //   }, {
-  //     name: 'MOTOROLA XOOM™',
-  //     snippet: 'The Next, Next Generation tablet.'
-  //   }
-  // ];
+  $scope.empData = {
+    eid: 101,
+    firstName: 'Sonu',
+    phones: [9876543210, 6789012345],
+    address: {
+      street: 'Gachibowli road',
+      city: 'Hyderabad', pin: 500081
+    }
+  };
+
+  $scope.phones = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
 });
 
 
