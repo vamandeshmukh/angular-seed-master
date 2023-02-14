3f73c3c34e332edf06057391e1af1ef2ed0d970f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-02-14 10:42:08 +0530

    angular

--- app/laptop-list/laptop-list.component.js
+++ app/laptop-list/laptop-list.component.js
@@ -1,30 +1,15 @@
+
 'use strict';
 
 angular.module('laptopList', []);
-
-const LaptopListController = ($scope) => {
-    $scope.empData = { city: 'Chennai' };
-};
 
 angular.
     module('laptopList').
     component('laptopList', {
         templateUrl: 'laptop-list/laptop-list.template.html',
-        controller: LaptopListController
+        controller: function LaptopListController() {
+
+        }
     });
-
-
-    // 'use strict';
-
-// angular.module('laptopList', []);
-
-// angular.
-//     module('laptopList').
-//     component('laptopList', {
-//         templateUrl: 'laptop-list/laptop-list.template.html',
-//         controller: function LaptopListController() {
-
-//         }
-//     });
 
 
