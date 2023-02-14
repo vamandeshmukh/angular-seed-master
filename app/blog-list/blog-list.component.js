77933b82eeb53e5be26961278f936ff92950a96a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-02-14 17:30:29 +0530

    angular

--- app/blog-list/blog-list.component.js
+++ app/blog-list/blog-list.component.js
@@ -1,19 +1,38 @@
 'use strict';
 
-const dataUrl = 'https://jsonplaceholder.typicode.com/posts';
+const dataUrl = 'https://jsonplaceholder.typicode.com/posts/';
 
 const BlogListController = ($scope, $http) => {
 
     $scope.blogData = [];
+    $scope.blogId = '';
+    $scope.currentBlog = {};
 
-    $http.get(dataUrl)
-        .then((resp) => {
-            $scope.blogData = resp.data;
-            console.log(resp.data);
-        })
-        .catch(err => console.log(err));
+    $scope.searchAllBlogs = () => {
+        $http.get(dataUrl)
+            .then((resp) => {
+                $scope.blogData = resp.data;
+                console.log(resp.data);
+                $scope.blogId = '';
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    $scope.searchBlogById = () => {
+        $http.get(dataUrl + $scope.blogId)
+            .then((resp) => {
+                $scope.currentBlog = resp.data;
+                console.log(resp.data);
+            })
+            .catch((err) => {
+                alert(`Blog with the id ${$scope.blogId} was not found!`);
+                console.log(err);
+                $scope.blogId = '';
+                $scope.currentBlog = {};
+            });
+    };
 };
-
 
 angular.module('blogList', []);
 
