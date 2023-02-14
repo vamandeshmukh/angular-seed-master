b28b020cdf0a81eae577a145f98e64833bdbbcee
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-02-14 17:01:51 +0530

    angular

--- app/blog-list/blog-list.component.js
+++ app/blog-list/blog-list.component.js
@@ -7,19 +7,11 @@ const BlogListController = ($scope, $http) => {
     $scope.blogData = [];
 
     $http.get(dataUrl)
-        // .then((res) => { // res })
-        .then(resp => {
+        .then((resp) => {
             $scope.blogData = resp.data;
             console.log(resp.data);
         })
         .catch(err => console.log(err));
-
-
-
-
-    // $http.post(dataUrl, {})
-    //     .then()
-    //     .catch();
 };
 
 
