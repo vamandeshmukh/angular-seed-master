94e2302f43b059fc65bf9d0dc03e06408cb5b3ea
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-02-14 16:55:26 +0530

    angular

--- app/blog-list/blog-list.component.js
+++ app/blog-list/blog-list.component.js
@@ -4,8 +4,15 @@ const dataUrl = 'https://jsonplaceholder.typicode.com/posts';
 
 const BlogListController = ($scope, $http) => {
 
-    
-    
+    $http.get(dataUrl)
+        .then(resp => console.log(resp.data))
+        .catch(err => console.log(err));
+
+    // $http.post(dataUrl, {})
+    //     .then()
+    //     .catch();
+
+
 };
 
 
